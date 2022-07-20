ceecb7768e0ae1a25f33065ad7527133656b1d47
Samet Burhan <samet26burhan2@gmail.com>
2022-07-20 15:48:11 +0300

    add some points

--- lesson9/src/lesson9.c
+++ lesson9/src/lesson9.c
@@ -57,4 +57,130 @@ int main(){
 		printf("%c",arr[i]);
 	}
 	return 0;
+}------------------------------------------------------------
+int main(){
+	int k,i;
+	scanf("%d",&k);
+	int arr[k];
+	for(i=0;i<k;i++){
+		scanf("%d",&arr[i]);
+	}
+	int p = 0;
+	while(p<k){
+	for(i=0;i<k;i++){
+		printf("%d ",arr[i]);
+
+	}
+	printf("\n");
+	    p++;
+	}
+
+	return 0;//
+}------------------------------------------------------------
+#include <stdio.h>
+
+int main(){
+	int k,i;
+	scanf("%d",&k);
+	int arr1[k];
+	int arr2[k];
+	for(i=0;i<=k-1;i++){
+		scanf("%d",&arr1[i]);
+		arr2[k-i-1]=arr1[i];
+	}
+
+	int p = 0;
+	while(p<k){
+	    if(p%2==0){
+	for(i=0;i<k;i++){
+		printf("%d ",arr2[i]);
+    } }
+    if(p%2==1){
+	for(i=0;i<k;i++){
+		printf("%d ",arr1[i]);
+    }
+	}
+	printf("\n");
+	    p++;
+	}
+
+	return 0;//
+}------------------------------------------------------------
+int main(){
+	int abc[3],i;//array listler bu şekilde tanımlanabilir
+	for(i=0;i<3;i++){
+		abc[i]=i+1;
+		printf("%d",abc[i]);
+	}
+
+	return 0;
+}------------------------------------------------------------
+int main(){
+	int k =15,i;
+
+	int arr1[k];
+	int arr2[k];
+	for(i=0;i<=k-1;i++){
+		arr1[i]=i+1;
+		arr2[k-i-1]=arr1[i];
+	}
+
+	int p = 0;
+	while(p<k){
+	    if(p%2==0){
+	for(i=0;i<k;i++){
+		printf("%d ",arr2[i]);
+    } }
+    if(p%2==1){
+	for(i=0;i<k;i++){
+		printf("%d ",arr1[i]);
+    }
+	}
+	printf("\n");
+	    p++;
+	}
+
+	return 0;//
+}------------------------------------------------------------
+int main()
+{
+	int i;
+    int array1[101];
+    for(i= 0;i<101;i++){
+        array1[i] = i+1;
+    }
+	int array2[101] = {0};
+	int k=0;
+	for(i=0;i<101;i++){
+	    if(array1[i]%2==0){
+	    array2[i]=array1[i];
+	    k++;
+
+	}printf("%d ",array2[i]);
+	}
+
+    printf("\n%d tane çift sayı var",k);
+
+    return 0;
+    //int array[10]={2}; başlangıç değerine 2 sayısını ekler
+    //10 karakterli bir dizi olduğuğu içi geri kalan 9 karakter 0 dır
+}------------------------------------------------------------
+int main() {
+	int N, i;
+
+	scanf("%d ", &N);
+
+	int arr[N];
+
+	for(i = 0 ; i < N ; i++){
+		scanf("%d", &arr[i]);
+    }
+	for(i = 0 ; i < N ; i++){
+		if(arr[i]+arr[i]==arr[i+1])
+		printf("%d. değer %d. değerin 2 katıdır",i+1,i);
+	}
+
+	return 0;
 }------------------------------------------------------------*/
+
+
